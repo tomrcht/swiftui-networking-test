2c40f5b3b5050537963dc59cdb1ef6e58a9bf259
Tom Rochat <tom.roch4t@gmail.com>
2020-04-01 13:30:12 +0200

    Initial Commit

---
+++ networking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  networking
+//
+//  Created by Tom Rochat on 01/04/2020.
+//  Copyright © 2020 Tom Rochat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
